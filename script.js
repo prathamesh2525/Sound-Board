6f5f6c8f9b45e4f4f72a94624609856c36ed344e
prathamesh2525 <prathameshmadniwale31@gmail.com>
2022-12-18 15:42:42 +0530

    added more files..

---
+++ script.js
@@ -0,0 +1,22 @@
+const sounds = ["applause", "boo", "panic", "success", "tada", "wrong"]
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button")
+  btn.classList.add("btn")
+  btn.innerText = sound
+
+  btn.addEventListener("click", () => {
+    stopSongs()
+    document.getElementById(sound).play()
+  })
+
+  document.getElementById("buttons").appendChild(btn)
+})
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound)
+    song.pause()
+    song.currenTime = 0
+  })
+}
